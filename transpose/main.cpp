541b24018f35ba5908e6dde271f952968dada83c
Viraj <virajpc@pop-os.localdomain>
2022-03-13 22:01:54 -0400

    Adding the basic linear algebra functions

--- transpose/main.cpp
+++ transpose/main.cpp
@@ -5,9 +5,19 @@
 #include <random>
 #include <cassert>
 
-// #include "naivelinag.h"
+#include "naivelinalg.h"
 
 int main(int argc, char** argv){
     // benchmark the transpose performance.
+    if(argc < 3){
+        printf("Usage: ./transpose_runner array_size tile_size [num_trails]\n");
+        return 0;
+    }
+    int num_trials = 1;
+    int N = atoi(argv[1]);
+    int tile_size = atoi(argv[2]);
+    if(argc > 3) num_trials = atoi(argv[3]);
 
+    matrix A, B;
+    
 }
